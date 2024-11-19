1832c5c4ab6b1c677643ad5728eed348ec08623f
neotod <neotod@protonmail.com>
2024-11-19 10:33:09 +0330

    update parac

--- modules/paracnet.py
+++ modules/paracnet.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python
-
-import numpy as np
+import math
 
 import torch
 from torch import nn
+
 
 
 class Parasin(nn.Module):
@@ -30,80 +29,41 @@ class Parasin(nn.Module):
         scale=10.0,
         init_weights=True,
     ):
-        # def __init__(
-        # self, in_features, out_features, nf, bias=True, is_first=False, omega_0=30
-        # ):
         super().__init__()
 
-        self.nf = 25
+        self.nf = 5
         self.omega_0 = omega_0
         self.is_first = is_first
 
         self.in_features = in_features
         self.linear = nn.Linear(in_features, out_features, bias=bias)
-        # self.ws = nn.Parameter(torch.ones(self.nf), requires_grad=True)
 
-        # ws = omega_0 * torch.rand(self.nf)
-        # ws = omega_0 * torch.ones(self.nf)
-        ws = torch.arange(15, 15 + 25).float()
+        # Initialize weights and parameters
+        self.init_params()
+                
+    def init_params(self):
+        """
+        Initializes the parameters for the sinusoidal activations.
+        """
+        ws = self.omega_0 * torch.rand(self.nf)
         self.ws = nn.Parameter(ws, requires_grad=True)
-        self.phis = nn.Parameter(requires_grad=True)
-        self.bs = nn.Parameter(requires_grad=True)
-        self.init_weights()
 
-    def init_weights(self):
-        with torch.no_grad():
-            uniform_samples = torch.rand(self.nf)
+        # Initialize phases uniformly in the range [-π, π]
+        self.phis = nn.Parameter(
+            -math.pi + 2 * math.pi * torch.rand(self.nf), requires_grad=True
+        )
 
-            # Scale and shift the samples to the range [-π, π]
-            lower_bound = -torch.tensor([3.14159265358979323846])  # -π
-            upper_bound = torch.tensor([3.14159265358979323846])  # π
-            scaled_samples = lower_bound + (upper_bound - lower_bound) * uniform_samples
-
-            self.phis = nn.Parameter(scaled_samples, requires_grad=True)
-
-            # Mean and diversity for Laplace random variable Y
-            mean_y = 0
-            diversity_y = 2 / (4 * self.nf)
-            # Generate Laplace random variable Y
-            laplace_samples = torch.distributions.laplace.Laplace(
-                mean_y, diversity_y
-            ).sample((self.nf,))
-
-            # Compute C from Y
-            c_samples = torch.sign(laplace_samples) * torch.sqrt(
-                torch.abs(laplace_samples)
-            )
-            self.bs = nn.Parameter(c_samples, requires_grad=True)
-
-    def siren_init_weights(self):
-        with torch.no_grad():
-            if self.is_first:
-                self.linear.weight.uniform_(-1 / self.in_features, 1 / self.in_features)
-            else:
-                self.linear.weight.uniform_(
-                    -np.sqrt(6 / self.in_features) / self.omega_0,
-                    np.sqrt(6 / self.in_features) / self.omega_0,
-                )
+        # Initialize scale factors based on a Laplace distribution
+        diversity_y = 1 / (2 * self.nf)
+        laplace_samples = torch.distributions.Laplace(0, diversity_y).sample((self.nf,))
+        self.bs = nn.Parameter(torch.sign(laplace_samples) * torch.sqrt(torch.abs(laplace_samples)), requires_grad=True)
 
     def forward(self, input):
-        # return torch.sin(self.omega_0 * self.linear(input))
-        # temp = self.omega_0 * self.linear(input)
-        temp = self.linear(input)
-        # print(temp.shape)
-        return self.param_act(temp)
+        return self.param_act(self.linear(input))
 
     def param_act(self, linout):
-        ws, bs, phis = (self.ws, self.bs, self.phis)
-        linoutx = linout.unsqueeze(-1).repeat_interleave(ws.shape[0], dim=3)
-        wsx = ws.expand(linout.shape[0], linout.shape[1], linout.shape[2], -1)
-        bsx = bs.expand(linout.shape[0], linout.shape[1], linout.shape[2], -1)
-        phisx = phis.expand(linout.shape[0], linout.shape[1], linout.shape[2], -1)
-        temp = bsx * (torch.sin((wsx * linoutx) + phisx))
-        temp2 = torch.sum(temp, 3)
-        # print(f"Activation Call input size:{linout.shape}")
-        # print(f"Activation Call output size:{temp2.shape}")
-        return temp2
+        sinusoidal_modulation = self.bs * torch.sin(self.ws * linout.unsqueeze(-1) + self.phis)
+        return sinusoidal_modulation.sum(dim=-1)
 
     def apply_activation(self, x):
         y = torch.zeros_like(x)
